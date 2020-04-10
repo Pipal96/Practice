0feebb96ace7fae0ceb0f72c0939cc6a057c8f83
Pipal96 <mitrofanov.chudo@gmail.com>
2020-04-10 15:46:12 +0400

    feat: adds code for popup menu

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 const HeaderButton = document.getElementById("header__button");
 const HeaderMenu = document.getElementById("header__menu");
+const HeaderPopupMenu = document.getElementById("header__popup-menu");
+const PopupMenu = document.getElementById("menu__popup");
+const BodyPopup = document.querySelector(".body-contaier");
 
 /* HeaderMenu */
-HeaderMenu.addEventListener("click", function (e) {
+HeaderMenu.querySelector("a").addEventListener("click", function (e) {
     e.preventDefault();
-    HeaderMenu.classList.toggle("header__menu_active");
+    HeaderMenu.classList.add("header__menu_active")
+    showMenu();
+    showPopupBody();
 });
+
+HeaderPopupMenu.querySelector("a").addEventListener("click", function (e) {
+    e.preventDefault();
+    HeaderMenu.classList.remove("header__menu_active")
+    showMenu();
+    showPopupBody();
+});
+
+
+function showMenu() {
+    PopupMenu.classList.toggle("header__nav-active")
+}
+
+function showPopupBody() {
+    BodyPopup.classList.toggle("body-contaier-active")
+}
 
 /* HeaderButton */
 setInterval(function showAnimationHeader() {
