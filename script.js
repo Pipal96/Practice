0138ece8da7b280c1e8802ed819356019de4870f
Pipal96 <mitrofanov.chudo@gmail.com>
2020-04-14 14:07:42 +0400

    feat: adds code for blocks

--- script.js
+++ script.js
@@ -17,6 +17,21 @@ const InputRangeValueThree = document.getElementById("input-range-value-three");
 const InputRangeFour = document.getElementById("range-four");
 const InputRangeValueFour = document.getElementById("input-range-value-four");
 
+const InputRangeFive = document.getElementById("range-five");
+const InputRangeValueFive = document.getElementById("input-range-value-five");
+
+const InputRangeSix = document.getElementById("range-six");
+const InputRangeValueSix = document.getElementById("input-range-value-six");
+
+const InputRangeSeven = document.getElementById("range-seven");
+const InputRangeValueSeven = document.getElementById("input-range-value-seven");
+
+const InputRangeEight = document.getElementById("range-eight");
+const InputRangeValueEight = document.getElementById("input-range-value-eight");
+
+const InputRangeNine = document.getElementById("range-nine");
+const InputRangeValueNine = document.getElementById("input-range-value-nine");
+
 /* HeaderMenu */
 HeaderMenu.querySelector("a").addEventListener("click", function (e) {
     e.preventDefault();
@@ -80,4 +95,34 @@ InputRangeValueFour.innerHTML = InputRangeFour.value;
 
 InputRangeFour.oninput = function () {
     InputRangeValueFour.innerHTML = InputRangeFour.value;
+}
+
+InputRangeValueFive.innerHTML = InputRangeFive.value;
+
+InputRangeFive.oninput = function () {
+    InputRangeValueFive.innerHTML = InputRangeFive.value;
+}
+
+InputRangeValueSix.innerHTML = InputRangeSix.value;
+
+InputRangeSix.oninput = function () {
+    InputRangeValueSix.innerHTML = InputRangeSix.value;
+}
+
+InputRangeValueSeven.innerHTML = InputRangeSeven.value;
+
+InputRangeSeven.oninput = function () {
+    InputRangeValueSeven.innerHTML = InputRangeSeven.value;
+}
+
+InputRangeValueEight.innerHTML = InputRangeEight.value;
+
+InputRangeEight.oninput = function () {
+    InputRangeValueEight.innerHTML = InputRangeEight.value;
+}
+
+InputRangeValueNine.innerHTML = InputRangeNine.value;
+
+InputRangeNine.oninput = function () {
+    InputRangeValueNine.innerHTML = InputRangeNine.value;
 }
